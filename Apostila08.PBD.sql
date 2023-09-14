1ebe6541c56cfaf2b2c9b722c03fb3ce1de50d12
Hemilly Medeiros <hemilly.santos@fatec.sp.gov.br>
2023-09-14 14:40:11 -0300

    apostila08:um programa que gere um valor que represetasse Celsius e após converter para Fahrenheit

--- Apostila08.PBD.sql
+++ Apostila08.PBD.sql
@@ -16,3 +16,21 @@ BEGIN
 	n1 := (random() * 10);
 	RAISE NOTICE '%', n1;
 END $$;
+
+-- 1.3 Faça um programa que gere um valor real no intervalo [20, 30] que representa uma temperatura em graus Celsius. Faça a conversão para Fahrenheit e exiba
+DO $$
+DECLARE 
+	n1 NUMERIC (4,2);
+	limite_inferior NUMERIC := 20;
+	limite_superior NUMERIC := 30;
+	inicial NUMERIC := 0;
+	numero NUMERIC (5,2);
+	F NUMERIC (4,2);
+BEGIN
+	n1 := FLOOR(limite_inferior + random() *(limite_superior - limite_inferior + 1))::NUMERIC;
+    inicial := random();
+    numero := n1 + inicial;
+    F = (n1 * 1.8)+32;
+	RAISE NOTICE 'o numero gerado foi: % %',   numero , 'ºC';
+	RAISE NOTICE 'A conversão desse valor para Fahrenheit é: %', F;
+END $$
